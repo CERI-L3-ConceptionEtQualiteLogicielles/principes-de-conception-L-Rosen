be8f008f54974c644665ec24a486db921989c48b
Lucas Rosenzweig <xeit0w@gmail.com>
2025-02-13 09:08:12 +0100

    Interface Segregation : -void envoiEmail devrait faire partie d'une autre interface

--- src/main/java/exo1/ContactService.java
+++ src/main/java/exo1/ContactService.java
@@ -3,6 +3,6 @@ package exo1;
 public interface ContactService {
     void ajouteContact(Contact contact);
     void supprimeContact(Contact contact);
-    //void envoiEmail(Contact contact, String message); Single Responsibility
+    //void envoiEmail(Contact contact, String message); Single Responsibility / Interface segregation
     void afficheContacts();
 }
