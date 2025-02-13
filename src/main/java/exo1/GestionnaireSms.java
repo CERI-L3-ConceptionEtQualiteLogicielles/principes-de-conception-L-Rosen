b44b6d670143f25555dd3b2beb4942587268b56f
Lucas Rosenzweig <xeit0w@gmail.com>
2025-02-13 10:01:19 +0100

    Implémentation envoi de SMS

---
+++ src/main/java/exo1/GestionnaireSms.java
@@ -0,0 +1,7 @@
+package exo1;
+
+public class GestionnaireSms implements SmsService{
+    public void envoiSms(Contact contact, String message) {
+        //Envoi de SMS
+    }
+}
---
+++ src/main/java/exo1/SmsService.java
@@ -0,0 +1,5 @@
+package exo1;
+
+public interface SmsService {
+    void envoiSms(Contact contact, String message);
+}
