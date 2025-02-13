12c15f5195061684e9f0782957520900e2c3381f
Lucas Rosenzweig <xeit0w@gmail.com>
2025-02-13 11:16:55 +0100

    Implémentation de EnvoiPhoto qui implémente EnvoiService et instancie ServiceEnvoiPhoto

--- src/main/java/exo1/EnvoiPhoto.java
+++ src/main/java/exo1/EnvoiPhoto.java
@@ -1,8 +1,20 @@
 package exo1;
 
+/*
+Methode 1
 public class EnvoiPhoto extends ServiceEnvoiPhoto implements EnvoiService{
 
     public void envoi(Contact contact, String message) {
         envoiPhoto(contact,message);
+    }
+}
+*/
+
+//Methode 2
+public class EnvoiPhoto implements EnvoiService{
+    public ServiceEnvoiPhoto service = new ServiceEnvoiPhoto();
+
+    public void envoi(Contact contact, String message) {
+        service.envoiPhoto(contact,message);
     }
 }
